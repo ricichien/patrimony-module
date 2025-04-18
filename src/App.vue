44ff7945bf0bd2e91bfae4161e937cc279e9ad8c
Riciere Marcone <riciere@outlook.com.br>
2025-04-18 19:19:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import FilterComponent from './components/FilterComponent.vue'
+import TableComponent from './components/TableComponent.vue'
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import i18next from 'i18next';
+import { VueI18n } from 'vue-i18next'; 
+
+createApp(App).mount('#app')
